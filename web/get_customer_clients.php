3b9964992ad1fa2803df4c3009af7d814f2b0e4d
Sho <sho.maglasang@gmail.com>
2021-11-20 23:08:54 +0800

    Support pagination

--- web/get_customer_clients.php
+++ web/get_customer_clients.php
@@ -5,12 +5,14 @@ include_once('lib.inc');
 
 $mysqli = open_db($dp_ip, $db_user, $db_password, $db_name);
 $customer_id = (isset($_GET['customer_id']) && $_GET['customer_id'] != 'all') ? $_GET['customer_id'] : null;
+$devices_page_num = (isset($_GET['devices_page_num'])) ? intval($_GET['devices_page_num']) : 1;
 
 $devices = array();
 $users = array();
 $servers = array();
 
 $vpn_clients = get_vpn_clients($customer_id);
+
 foreach($vpn_clients as $vpn_client)
 {
   $client_info = get_client_info($vpn_client['common_name'], $vpn_client['customer_id']);
@@ -31,7 +33,35 @@ foreach($vpn_clients as $vpn_client)
   }
 }
 
-echo json_encode(array('devices' => $devices, 'users' => $users, 'servers' => $servers));
+$total_devices = count($devices);
+$total_users = count($users);
+$total_servers = count($servers);
+
+if ($devices_page_num > 1)
+{
+  $rem_front = ($devices_page_num -1) * $max_devices_per_page;
+  $devices = array_slice($devices, $rem_front);
+}
+
+if (count($devices) > $max_devices_per_page)
+{
+  $devices = array_slice($devices, 0, $max_devices_per_page);
+}
+
+echo json_encode(array(
+  'devices' => $devices,
+  'total_devices' => $total_devices,
+  'devices_per_page' => $max_devices_per_page,
+  'devices_page_num' => $devices_page_num,
+  'users' => $users,
+  'total_users' => $total_users,
+  'users_per_page' => $max_users_per_page,
+  'users_page_num' => 1,
+  'servers' => $servers,
+  'total_servers' => $total_servers,
+  'servers_per_page' => $max_servers_per_page,
+  'servers_page_num' => 1
+));
 
 close_db($mysqli);
 exit();
--- web/index.php
+++ web/index.php
@@ -32,7 +32,16 @@ echo '<div class="table_title">Devices</div>';
 echo '<div id="client_list"><table id="client_devices" class="online">
   <tr class="table_headers"><td>#&nbsp;&nbsp;&nbsp;</td><td>VPN Client Name</td><td>VPN IP</td><td>Expiry</td>
   <td>Public IP</td><td class="description">Description</td><td>Customer Name</td><td class="edit_device_col">Edit</td></tr>';
-echo '</table></div>';
+echo '</table>';
+echo '<div id="device_pagination" class="device_pagination" style="display:none;">';
+echo '<form id="page_form" style="display:none;">
+  <input type="hidden" name="devices_page_num" id="devices_page_num" value="1"/>
+  </form>';
+echo '<ul><li id="device_pagination_text"></li><li><a href="#" id="device_pagination_newer"><i class="fas fa-arrow-circle-left"></i></a></li>
+      <li><a href="#" id="device_pagination_older"><i class="fas fa-arrow-circle-right"></i></a></li>
+      </ul>';
+echo '</div>';
+echo '</div>';
 
 echo '<div id="online_servers" style="display:none;"><br/><br/><div class="table_title">Servers</div>';
 echo '<table id="client_servers" class="online">
@@ -84,26 +93,40 @@ echo "</body>
   });
 
   $('#customer_id').change(function(){
+    $('#devices_page_num').val(1);
     update_page();
   });
 
-  function update_page() {
-    $('.device_entry').remove();
-    $('.server_entry').remove();
-    $('.user_entry').remove();
+  function update_page(clear_devices = true, clear_servers = true, clear_users = true) {
+    if (clear_devices) {
+      $('.device_entry').remove();
+    }
+
+    if (clear_servers) {
+      $('.server_entry').remove();
+    }
+
+    if (clear_users) {
+      $('.user_entry').remove();
+    }
+
     var customer_id = $('#customer_id').val();
     $.ajax({
       url: 'get_customer_clients.php',
       type: 'GET',
       data: {
-        customer_id: customer_id
+        customer_id: customer_id,
+        devices_page_num: $('#devices_page_num').val()
       },
       cache: false,
       success: function(data){
         data = JSON.parse(data);
+        var devices_page_num = data['devices_page_num'];
+        var devices_per_page = data['devices_per_page'];
+        var devices_offset = (devices_page_num - 1) * devices_per_page;
         data['devices'].forEach(function (d, i){
           var tr_id = 'client_device_' + (i+1);
-          var row = '<tr class=\"device_entry\" id=\"' + tr_id + '\"><td>' + (i+1) + '</td>';
+          var row = '<tr class=\"device_entry\" id=\"' + tr_id + '\"><td>' + (i+1+devices_offset) + '</td>';
           row += '<td>' + d['common_name'] + '</td>';
           row += '<td>' + d['ip'] + '</td>';
           row += '<td>' + d['cert_expiry'] + '</td>';
@@ -119,56 +142,142 @@ echo "</body>
           $('#client_devices tr:last').after(row);
         });
 
-        if (data['servers'].length > 0) {
-          data['servers'].forEach(function (d, i){
-            var tr_id = 'client_server_' + (i+1);
-            var row = '<tr class=\"server_entry\" id=\"' + tr_id + '\"><td>' + (i+1) + '</td>';
-            row += '<td>' + d['common_name'] + '</td>';
-            row += '<td>' + d['ip'] + '</td>';
-            row += '<td>' + d['cert_expiry'] + '</td>';
-            row += '<td>' + d['public_ip'] + '</td>';
-            row += '<td class=\"description\">' + d['description'] + '</td>';
-            row += '<td>' + d['customer_name'] + '</td>';
-            row += '<td><a href=\"#\" class=\"edit_device_cell\" data-toggle=\"modal\" data-target=\"#loadEditClientModal\" ' +
-              'data-customer_id=\"' + d['customer_id'] + '\" data-client_name=\"' + d['common_name'] + '\" ' +
-              'data-customer_name=\"' + d['customer_name'] + '\" data-client_description=\"' + d['description'] + '\" ' +
-              'data-row_id=\"' + tr_id + '\" ' +
-              '><i class=\"far fa-edit\"></i></a></td>';
-            row += '</tr>';
-            $('#client_servers tr:last').after(row);
-          });
-          $('#online_servers').show();
-        } else {
-          $('#online_servers').hide();
+        update_pagination('devices', data['total_devices'], data['devices_page_num'], data['devices_per_page']);
+
+        if (clear_servers) {
+          if (data['servers'].length > 0) {
+            data['servers'].forEach(function (d, i){
+              var tr_id = 'client_server_' + (i+1);
+              var row = '<tr class=\"server_entry\" id=\"' + tr_id + '\"><td>' + (i+1) + '</td>';
+              row += '<td>' + d['common_name'] + '</td>';
+              row += '<td>' + d['ip'] + '</td>';
+              row += '<td>' + d['cert_expiry'] + '</td>';
+              row += '<td>' + d['public_ip'] + '</td>';
+              row += '<td class=\"description\">' + d['description'] + '</td>';
+              row += '<td>' + d['customer_name'] + '</td>';
+              row += '<td><a href=\"#\" class=\"edit_device_cell\" data-toggle=\"modal\" data-target=\"#loadEditClientModal\" ' +
+                'data-customer_id=\"' + d['customer_id'] + '\" data-client_name=\"' + d['common_name'] + '\" ' +
+                'data-customer_name=\"' + d['customer_name'] + '\" data-client_description=\"' + d['description'] + '\" ' +
+                'data-row_id=\"' + tr_id + '\" ' +
+                '><i class=\"far fa-edit\"></i></a></td>';
+              row += '</tr>';
+              $('#client_servers tr:last').after(row);
+            });
+            $('#online_servers').show();
+          } else {
+            $('#online_servers').hide();
+          }
         }
 
-        if (data['users'].length > 0) {
-          data['users'].forEach(function (d, i){
-            var tr_id = 'client_user_' + (i+1);
-            var row = '<tr class=\"user_entry\" id=\"' + tr_id + '\"><td>' + (i+1) + '</td>';
-            row += '<td>' + d['common_name'] + '</td>';
-            row += '<td>' + d['ip'] + '</td>';
-            row += '<td>' + d['cert_expiry'] + '</td>';
-            row += '<td>' + d['public_ip'] + '</td>';
-            row += '<td class=\"description\">' + d['description'] + '</td>';
-            row += '<td>' + d['customer_name'] + '</td>';
-            row += '<td><a href=\"#\" class=\"edit_device_cell\" data-toggle=\"modal\" data-target=\"#loadEditClientModal\" ' +
-              'data-customer_id=\"' + d['customer_id'] + '\" data-client_name=\"' + d['common_name'] + '\" ' +
-              'data-customer_name=\"' + d['customer_name'] + '\" data-client_description=\"' + d['description'] + '\" ' +
-              'data-row_id=\"' + tr_id + '\" ' +
-              '><i class=\"far fa-edit\"></i></a></td>';
-            row += '</tr>';
-            $('#client_users tr:last').after(row);
-          });
-          $('#online_users').show();
-        } else {
-          $('#online_users').hide();
+        if (clear_users) {
+          if (data['users'].length > 0) {
+            data['users'].forEach(function (d, i){
+              var tr_id = 'client_user_' + (i+1);
+              var row = '<tr class=\"user_entry\" id=\"' + tr_id + '\"><td>' + (i+1) + '</td>';
+              row += '<td>' + d['common_name'] + '</td>';
+              row += '<td>' + d['ip'] + '</td>';
+              row += '<td>' + d['cert_expiry'] + '</td>';
+              row += '<td>' + d['public_ip'] + '</td>';
+              row += '<td class=\"description\">' + d['description'] + '</td>';
+              row += '<td>' + d['customer_name'] + '</td>';
+              row += '<td><a href=\"#\" class=\"edit_device_cell\" data-toggle=\"modal\" data-target=\"#loadEditClientModal\" ' +
+                'data-customer_id=\"' + d['customer_id'] + '\" data-client_name=\"' + d['common_name'] + '\" ' +
+                'data-customer_name=\"' + d['customer_name'] + '\" data-client_description=\"' + d['description'] + '\" ' +
+                'data-row_id=\"' + tr_id + '\" ' +
+                '><i class=\"far fa-edit\"></i></a></td>';
+              row += '</tr>';
+              $('#client_users tr:last').after(row);
+            });
+            $('#online_users').show();
+          } else {
+            $('#online_users').hide();
+          }
         }
       },
       error: function(data){
       }
     });
   }
+
+
+  function update_pagination(type, num_entries, page_num, page_limit)
+  {
+    console.log('type:');
+    console.log(type);
+    console.log('num_entries:');
+    console.log(num_entries);
+    console.log('page_num:');
+    console.log(page_num);
+    console.log('page_limit:');
+    console.log(page_limit);
+
+    var start, end, s;
+    if (page_num > 1) {
+      start = ((page_num - 1) * page_limit) + 1;
+    } else {
+      start = 1;
+    }
+
+    if (num_entries > page_limit) {
+      end = page_limit * page_num;
+      if (end > num_entries) {
+        end = num_entries;
+      }
+
+      s = start + ' - ' + end + ' of ' + num_entries;
+    } else {
+      end = num_entries;
+      s = start + ' - ' + end;
+    }
+
+    console.log('start:');
+    console.log(start);
+    console.log('end:');
+    console.log(end);
+    console.log('s:');
+    console.log(s);
+
+    if (type == 'devices') {
+      if (num_entries > 0) {
+        $('#device_pagination_text').text(s);
+        $('#device_pagination').show();
+      } else {
+        $('#device_pagination').hide();
+      }
+
+      if (page_num > 1) {
+        $('#device_pagination_newer').removeClass('disable-click');
+        if ((page_num * page_limit) >= num_entries) {
+          $('#device_pagination_older').addClass('disable-click');
+        } else {
+          $('#device_pagination_older').removeClass('disable-click');
+        }
+      } else {
+        $('#device_pagination_newer').addClass('disable-click');
+        if (num_entries > page_limit) {
+          $('#device_pagination_older').removeClass('disable-click');
+        } else {
+          $('#device_pagination_older').addClass('disable-click');
+        }
+      }
+
+      $('#devices_page_num').val(page_num);
+    }
+  }
+
+  $('#device_pagination_newer').click(function() {
+    var page_num = $('#devices_page_num').val();
+    page_num--;
+    $('#devices_page_num').val(page_num);
+    update_page(true, false, false);
+  });
+
+  $('#device_pagination_older').click(function() {
+    var page_num = $('#devices_page_num').val();
+    page_num++;
+    $('#devices_page_num').val(page_num);
+    update_page(true, false, false);
+  });
 
   $('#loadEditClientModal').on('show.bs.modal', function (event) {
     var a = $(event.relatedTarget);
